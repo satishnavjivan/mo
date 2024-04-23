89fc2b0a0ed0e2ebc0bd98c3828b1906183414cc
satishnavjivan <satish.dharavia@multicodes.in>
2024-04-23 16:44:44 +0530

    shop 1

--- pages/c/[...cat_slug].js
+++ pages/c/[...cat_slug].js
@@ -13,23 +13,78 @@ import Products from '../../src/components/products';
 import Category from '../../src/components/categories/category';
 import Image from 'next/image';
 import Link from 'next/link';
+import Loader from "../../src/components/loaderspin";
+import { useEffect, useState } from 'react';
 
-export default function cat_slug({ headerFooter, categories, cacheValid }) {
-    //console.log('params',params);
+export default function cat_slug({ headerFooter ,params}) {
+    console.log('params',params.cat_slug);
     //console.log('products',products);
+    const [categories,setCategories]=useState({products:{},cat_list:{},cat_data:{}});
+	const [loading, SetLoading] = useState(true);
+
     const { products } = categories;
     const { cat_list } = categories;
     const { cat_data } = categories;
     const { single_category_image, single_category_image_url } = headerFooter?.footer?.options ?? '';
     // Banner 
     const { category_image_select_category_section } = headerFooter?.footer?.options ?? '';
+    
+    let cacheValid = 0;
+
+    useEffect(()=>{
+        (async () => {
+                    const { cat_slug } = params || {};
+                    var lastSlug = cat_slug[Object.keys(cat_slug)[Object.keys(cat_slug).length - 1]];
+                    let rsCat = null;
+                    await axios.get(SHOP_CATEGORIES_CAT_SLUG_CACHE + 'product_cat_' + lastSlug + '.js')
+                    .then((response) => {
+                        console.log('response',response.data);
+                          if (response.data?.products != undefined) {
+                            cacheValid = 1;
+                            setCategories(response.data);
+                            }
+                        SetLoading(false);
+                    })
+                    .catch((error) => {
+                        console.log(error.response.data);
+                        });
+
+                    if(cacheValid == 0)  
+                    {
+                        await axios.get(SHOP_CATEGORIES_CAT_SLUG+'?cat_slug='+lastSlug)
+                        .then((response) => {
+                            console.log('response 2222',response.data);
+                              if (response.data?.products != undefined) {
+                                cacheValid = 0;
+                                setCategories(response.data);
+                                }
+                            SetLoading(false);
+                        })
+                        .catch((error) => {
+                            console.log(error.response.data);
+                            });
+                    }
+                    
+
+                
+        })();
+    }, [params.cat_slug])
+
+
     console.log('categories', categories);
     console.log('cacheValid', cacheValid);
     console.log('headerFooter', single_category_image);
     console.log('category_image_select_category_section', category_image_select_category_section);
     //console.log('headerFooter',headerFooter?.footer?.options?.single_category_image_url);
-
-    if (isEmpty(products) && isEmpty(cat_data)) {
+    if(loading)
+    {
+         return (
+            <Layout headerFooter={headerFooter || {}}>
+                <Loader />
+            </Layout>
+        )
+    }
+    else if (isEmpty(products) && isEmpty(cat_data)) {
         return (
             <Layout headerFooter={headerFooter || {}}>
                 Data Not found
@@ -103,6 +158,7 @@ export async function getServerSideProps(context) {
     const { data: headerFooterData } = await axios.get(HEADER_FOOTER_ENDPOINT);
     // const {data : res} = await axios.get(SHOP_PRODUCTLIST_BY_PARAMETER,context);
     const { params } = context;
+    /*const { params } = context;
     const { cat_slug } = params || {};
     var lastSlug = cat_slug[Object.keys(cat_slug)[Object.keys(cat_slug).length - 1]];
     let rsCat = null;
@@ -115,15 +171,16 @@ export async function getServerSideProps(context) {
         const { data: res_cat } = await axios.get(SHOP_CATEGORIES_CAT_SLUG, context);
         rsCat = res_cat;
         cacheValid = 0;
-    }
+    }*/
 
     // Return the ID to the component
     return {
         props: {
             headerFooter: headerFooterData?.data ?? {},
             //products: res,
-            categories: rsCat,
-            cacheValid: cacheValid,
+           // categories: rsCat,
+            //cacheValid: cacheValid,
+            params: params,
         },
     };
 };
